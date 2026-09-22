ea549d15e42d626d027b98f26883fdcc4d0b9e9b
Jonathan <jonathanhsu198@gmail.com>
2026-09-22 09:30:45 -0400

    Update monkeys.cpp

---
+++ C++/cppf6/monkeys.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+std::string monkeys(std::string word);
+
+
+int main() {
+    srand(time(0));
+}
+//char(97)
+//insert(int index, int quantity, char newChar)
